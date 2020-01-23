012fcdfccdbc94b1fc02101e69ab3eda7a513157
ryujisuda <cherryhee_@naver.com>
2020-01-24 03:28:40 +0900

    스크롤 뷰 추가, 메인 화면 완성.

--- app/src/main/java/com/inuappcenter/shareu/activity/MainActivity.java
+++ app/src/main/java/com/inuappcenter/shareu/activity/MainActivity.java
@@ -10,7 +10,9 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.inuappcenter.shareu.R;
+import com.inuappcenter.shareu.model.Notice;
 import com.inuappcenter.shareu.model.SuperiorLecture;
+import com.inuappcenter.shareu.recycler.NoticeAdapter;
 import com.inuappcenter.shareu.recycler.SuperiorLectureAdapter;
 import com.kingfisher.easyviewindicator.RecyclerViewIndicator;
 
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView=(RecyclerView)findViewById(R.id.recyclerview_main);
         RecyclerViewIndicator horizontalIndicator = (RecyclerViewIndicator)findViewById(R.id.recyclerViewIndicator);
+
         final EditText editText =(EditText)findViewById(R.id.etv_main);
         LinearLayoutManager layoutManager=new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
         recyclerView.setHasFixedSize(true);
@@ -47,6 +50,26 @@ public class MainActivity extends AppCompatActivity {
         horizontalIndicator.setRecyclerView(recyclerView);
 
 
+        RecyclerView recyclerView2=(RecyclerView)findViewById(R.id.recyclerview2_main);
+        LinearLayoutManager layoutManager2=new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
+        recyclerView2.setHasFixedSize(true);
+        recyclerView2.setLayoutManager(layoutManager2);
 
+        ArrayList<Notice> items2=new ArrayList<>();
+        Notice[] item2=new Notice[10];
+        item2[0]=new Notice("1번 공지",R.drawable.rightarrow);
+        item2[1]=new Notice("2번 공지",R.drawable.rightarrow);
+        item2[2]=new Notice("3번 공지",R.drawable.rightarrow);
+        item2[3]=new Notice("4번 공지",R.drawable.rightarrow);
+        item2[4]=new Notice("5번 공지",R.drawable.rightarrow);
+        item2[5]=new Notice("6번 공지",R.drawable.rightarrow);
+        item2[6]=new Notice("7번 공지",R.drawable.rightarrow);
+        item2[7]=new Notice("8번 공지",R.drawable.rightarrow);
+        item2[8]=new Notice("9번 공지",R.drawable.rightarrow);
+        item2[9]=new Notice("10번 공지",R.drawable.rightarrow);
+
+        for(int i=0;i<10;i++) items2.add(item2[i]);
+
+        recyclerView2.setAdapter(new NoticeAdapter(getApplicationContext(),items2));
     }
 }
