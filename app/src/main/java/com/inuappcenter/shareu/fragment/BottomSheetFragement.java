8459239aadffdb96970b61acf07ae02db266fa4d
ryujisuda <cherryhee_@naver.com>
2020-02-23 01:12:21 +0900

    bottom sheet 디자인 추가.

--- app/src/main/java/com/inuappcenter/shareu/fragment/BottomSheetFragement.java
+++ app/src/main/java/com/inuappcenter/shareu/fragment/BottomSheetFragement.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.androidadvance.topsnackbar.TSnackbar;
 import com.deishelon.roundedbottomsheet.RoundedBottomSheetDialogFragment;
@@ -42,6 +43,7 @@ public class BottomSheetFragement extends RoundedBottomSheetDialogFragment{
     private EditText etv_search;
     private ImageButton  etv_search_click;
     private TSnackbar snackbar;
+    private TextView tv_search_please;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -70,17 +72,27 @@ public class BottomSheetFragement extends RoundedBottomSheetDialogFragment{
                         {
                             if(response.isSuccessful())
                             {
-                                dataList=new ArrayList<>();
-                                for(int i=0;i<response.body().size();i++)
+                                if(response.body().size()==0)
                                 {
-                                    dataList.add(new subjectName(response.body().get(i).getSubjectName()));
+                                    tv_search_please.setVisibility(View.VISIBLE);
+                                    tv_search_please.setText("검색 결과가 없습니다 :(");
+                                }
+                                else
+                                {
+                                    tv_search_please.setVisibility(View.GONE);
+                                    dataList=new ArrayList<>();
+                                    for(int i=0;i<response.body().size();i++)
+                                    {
+                                        dataList.add(new subjectName(response.body().get(i).getSubjectName()));
+                                    }
+                                    recyclerView.setHasFixedSize(true);
+                                    recyclerView.setLayoutManager(manager); // LayoutManager 등록
+                                    recyclerView.setAdapter(new BottomSheetAdapter(dataList,getActivity(),(OnItemClick)(getActivity())));  // Adapter 등록
                                 }
 
                             }
 
-                            recyclerView.setHasFixedSize(true);
-                            recyclerView.setLayoutManager(manager); // LayoutManager 등록
-                            recyclerView.setAdapter(new BottomSheetAdapter(dataList,getActivity(),(OnItemClick)(getActivity())));  // Adapter 등록
+
                         }
                         @Override
                         public void onFailure(Call<List<subjectName>> call, Throwable t) {
@@ -97,7 +109,7 @@ public class BottomSheetFragement extends RoundedBottomSheetDialogFragment{
     }
     public void init(View view)
     {
-
+        tv_search_please = view.findViewById(R.id.tv_search_please);
         recyclerView = view.findViewById(R.id.recyclerview_bottomsheet);
         manager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         etv_search = view.findViewById(R.id.etv_search);
--- app/src/main/java/com/inuappcenter/shareu/fragment/BottomSheetFragment2.java
+++ app/src/main/java/com/inuappcenter/shareu/fragment/BottomSheetFragment2.java
@@ -40,13 +40,14 @@ public class BottomSheetFragment2 extends RoundedBottomSheetDialogFragment {
     private RetrofitService networkService;
     private EditText etv_search;
     private ImageButton  etv_search_click;
-    private TSnackbar snackbar;
+    private TextView tv_search_please;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_bottomsheet,container);
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         init(view);
+
         etv_search_click.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -55,19 +56,28 @@ public class BottomSheetFragment2 extends RoundedBottomSheetDialogFragment {
                     @Override
                     public void onResponse(Call<List<profName> > call, Response<List<profName>> response)
                     {
-                        if(response.isSuccessful())
+                        if(response.body().size()==0)
                         {
-                            dataList=new ArrayList<>();
-                            for(int i=0;i<response.body().size();i++)
+                            tv_search_please.setVisibility(View.VISIBLE);
+                            tv_search_please.setText("검색 결과가 없습니다 :(");
+                        }
+                        else
+                        {
+                            tv_search_please.setVisibility(View.GONE);
+                            if(response.isSuccessful())
                             {
-                                dataList.add(new profName(response.body().get(i).getProfName()));
-                            }
+                                dataList=new ArrayList<>();
+                                for(int i=0;i<response.body().size();i++)
+                                {
+                                    dataList.add(new profName(response.body().get(i).getProfName()));
+                                }
 
+                            }
+                            recyclerView.setHasFixedSize(true);
+                            recyclerView.setLayoutManager(manager); // LayoutManager 등록
+                            recyclerView.setAdapter(new BottomSheetAdapter2(dataList,getActivity(),(OnItemClick)(getActivity())));  // Adapter 등록
                         }
 
-                        recyclerView.setHasFixedSize(true);
-                        recyclerView.setLayoutManager(manager); // LayoutManager 등록
-                        recyclerView.setAdapter(new BottomSheetAdapter2(dataList,getActivity(),(OnItemClick)(getActivity())));  // Adapter 등록
                     }
                     @Override
                     public void onFailure(Call<List<profName>> call, Throwable t)
@@ -82,10 +92,12 @@ public class BottomSheetFragment2 extends RoundedBottomSheetDialogFragment {
     }
     public void init(View view)
     {
+        tv_search_please = view.findViewById(R.id.tv_search_please);
         recyclerView = view.findViewById(R.id.recyclerview_bottomsheet);
         manager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         etv_search = view.findViewById(R.id.etv_search);
         etv_search_click = view.findViewById(R.id.etv_search_click);
+        tv_search_please.setText("교수명을 검색해주세요 :)");
 
     }
 
